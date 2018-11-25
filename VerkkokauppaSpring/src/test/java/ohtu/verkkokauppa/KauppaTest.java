32f0ebcb0c180ae9113d4de2381d1aa3301a42d9
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-11-25 16:39:30 +0200

    Q5

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -101,4 +101,73 @@ public class KauppaTest {
 
 	}
 
+	@Test
+	public void poistaKorista() {
+		when(viite.uusi()).thenReturn(42);
+		when(varasto.saldo(1)).thenReturn(10); 
+		when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+		when(varasto.saldo(2)).thenReturn(10); 
+		when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "maito2", 5));
+
+		Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+		k.aloitaAsiointi();
+		k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+		k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+		k.poistaKorista(2);
+		k.tilimaksu("pekka", "12345");
+
+		// sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+		verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);   
+
+	}
+
+	@Test
+	public void aloitaNollaa() {
+		when(viite.uusi()).thenReturn(42);
+		when(varasto.saldo(1)).thenReturn(10); 
+		when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+		when(varasto.saldo(2)).thenReturn(10); 
+		when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "maito2", 5));
+
+		Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+		k.aloitaAsiointi();
+		k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+		k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+		k.poistaKorista(2);
+
+		k.aloitaAsiointi();
+		k.tilimaksu("pekka", "12345");
+
+		// sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+		verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 0);   
+	}
+
+	@Test
+	public void uusiViite() {
+		when(viite.uusi()).thenReturn(42);
+		when(varasto.saldo(1)).thenReturn(10); 
+		when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+		when(varasto.saldo(2)).thenReturn(10); 
+		when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "maito2", 5));
+
+		Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+		k.aloitaAsiointi();
+		k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+		k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+		k.poistaKorista(2);
+
+		k.aloitaAsiointi();
+		k.tilimaksu("pekka", "12345");
+
+		// sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+		k.tilimaksu("pekka", "12345");
+		verify(viite, times(2)).uusi();
+	}
+	/*
+	varmistettava, että metodin aloitaAsiointi kutsuminen nollaa edellisen ostoksen tiedot (eli edellisen ostoksen hinta ei näy uuden ostoksen hinnassa), katso tarvittaessa apua projektin MockitoDemo testeistä!
+varmistettava, että kauppa pyytää uuden viitenumeron jokaiselle maksutapahtumalle, katso tarvittaessa apua projektin MockitoDemo testeistä!
+	*/
 }
